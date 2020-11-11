0ea4b5d68044bb16333d0732450a1688d9f31d4e
min950903 <min950903@gmail.com>
2020-11-11 23:57:30 +0900

    update login header css

--- src/app.jsx
+++ src/app.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import './app.css';
 import { Switch, Route } from 'react-router-dom';
 import Login from './pages/login';
+import styles from './components/login/login.module.css';
 
 function App() {
   return (
     <Switch>
       <Route path='/login'>
-        <Login />
+        <div className={styles.container}>
+          <Login />
+        </div>
       </Route>
     </Switch>
   );
