8fe6dd686014d5d8acbd660b65adae40212d2c65
min950903 <min950903@gmail.com>
2020-11-13 23:58:29 +0900

    add grid css in card-maker

--- src/components/card/cardMaker.jsx
+++ src/components/card/cardMaker.jsx
@@ -4,6 +4,22 @@ const CardMaker = () => {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Card Maker</h1>
+      <form className={styles.grid}>
+        <input className={styles.name} placeholder='Name' />
+        <input placeholder='Company' />
+        <select>
+          <option value='makerLight' selected>
+            Light
+          </option>
+          <option value='makerBlack'>Black</option>
+          <option value='makerColorful'>Colorful</option>
+        </select>
+        <input className={styles.title} placeholder='Title' />
+        <input placeholder='Email' />
+        <input className={styles.message} placeholder='Message' />
+        <button>No file</button>
+        <button>Add</button>
+      </form>
     </div>
   );
 };
