4ab1be50de00203fe3723eff1a776de2f2674a1d
min950903 <min950903@gmail.com>
2020-11-15 23:34:40 +0900

    create cardMaker item  component, css

---
+++ src/components/card/cardItem.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import styles from '../../assets/css/card/item.module.css';
+
+const CardItem = () => {
+  return (
+    <form className={styles.container}>
+      <input className={styles.name} placeholder='Name' />
+      <input className={styles.company} placeholder='Company' />
+      <select className={styles.color}>
+        <option value='makerLight' selected>
+          Light
+        </option>
+        <option value='makerBlack'>Black</option>
+        <option value='makerColorful'>Colorful</option>
+      </select>
+      <input className={styles.title} placeholder='Title' />
+      <input className={styles.email} placeholder='Email' />
+      <textarea className={styles.message} placeholder='Message' />
+      <button className={styles.fileBtn}>No file</button>
+      <button className={styles.addBtn}>Add</button>
+    </form>
+  );
+};
+
+export default CardItem;
--- src/components/card/cardMaker.jsx
+++ src/components/card/cardMaker.jsx
@@ -1,25 +1,12 @@
 import React from 'react';
 import styles from '../../assets/css/card/maker.module.css';
+import CardItem from './cardItem';
+
 const CardMaker = () => {
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>Card Maker</h1>
-      <form className={styles.grid}>
-        <input className={styles.name} placeholder='Name' />
-        <input placeholder='Company' />
-        <select>
-          <option value='makerLight' selected>
-            Light
-          </option>
-          <option value='makerBlack'>Black</option>
-          <option value='makerColorful'>Colorful</option>
-        </select>
-        <input className={styles.title} placeholder='Title' />
-        <input placeholder='Email' />
-        <input className={styles.message} placeholder='Message' />
-        <button>No file</button>
-        <button>Add</button>
-      </form>
+      <h1 className={styles.cardTitle}>Card Maker</h1>
+      <CardItem />
     </div>
   );
 };
